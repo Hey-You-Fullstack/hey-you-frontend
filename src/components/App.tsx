6a3dfe2aa62de6d93c2e1cd2a1f65ee2b083da60
rektdeckard <friedtm@gmail.com>
2020-08-04 23:00:54 -0400

    Refactor component imports

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,12 @@
 import React from "react";
 
-import logo from "../assets/Hand.png";
 import "./App.css";
-import Scroller from "./Scroller/Scroller";
+import { Header, Scroller } from "./";
 
 function App() {
   return (
     <div className="app">
-      <header className="app-header">
-        <img
-          className="app-logo"
-          src={logo}
-          alt="sketchy outline of a hand in red"
-        />
-        <h1 className="app-title">Hey, You</h1>
-      </header>
+      <Header />
       <Scroller />
     </div>
   );
