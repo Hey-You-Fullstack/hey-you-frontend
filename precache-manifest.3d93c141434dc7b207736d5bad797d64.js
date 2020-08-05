2cb1c3ca14ac2f65ed4e1392e5f48b75658166f4
rektdeckard <friedtm@gmail.com>
2020-08-04 23:09:04 -0400

    Updates

---
+++ precache-manifest.3d93c141434dc7b207736d5bad797d64.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2d10f3f4be2a9e004180a0395b03562",
+    "url": "/hey-you-frontend/index.html"
+  },
+  {
+    "revision": "e4dd6cb8900d6ef0a910",
+    "url": "/hey-you-frontend/static/css/main.833cfc9f.chunk.css"
+  },
+  {
+    "revision": "a29b03fcc094d0d46111",
+    "url": "/hey-you-frontend/static/js/2.f048b95a.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/hey-you-frontend/static/js/2.f048b95a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4dd6cb8900d6ef0a910",
+    "url": "/hey-you-frontend/static/js/main.677f0cdb.chunk.js"
+  },
+  {
+    "revision": "61c235706429471e4d4b",
+    "url": "/hey-you-frontend/static/js/runtime-main.0d7499a0.js"
+  },
+  {
+    "revision": "b961a29672f8272864d3d6ea74f2bf89",
+    "url": "/hey-you-frontend/static/media/Hand.b961a296.png"
+  },
+  {
+    "revision": "4bab6bc19856cd92fcb3b04f535d3bc2",
+    "url": "/hey-you-frontend/static/media/HandText.4bab6bc1.png"
+  }
+]);
