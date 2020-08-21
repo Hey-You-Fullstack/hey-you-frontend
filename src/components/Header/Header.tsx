af259c193f0a8e3e6a8b6dda4ce2d0631936f789
rektdeckard <friedtm@gmail.com>
2020-08-21 16:11:48 -0400

    Header: make progress bar fullwidth on all screens

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { motion, useSpring, useTransform, MotionValue } from "framer-motion";
+import { motion, useTransform, MotionValue } from "framer-motion";
 
 import logo from "../../assets/Hand.png";
 import "./Header.css";
@@ -10,10 +10,17 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ scrollProgress }) => {
   const [isComplete, setIsComplete] = useState<boolean>(false);
-  const yRange = useTransform(scrollProgress, [0, 0.9], [0, 1]);
-  const pathLength = useSpring(yRange, { stiffness: 400, damping: 90 });
+  const [width, setWidth] = useState<number>(0);
+  const yRange = useTransform(scrollProgress, [0, 0.9], [0, 100]);
 
-  useEffect(() => yRange.onChange((v) => setIsComplete(v >= 1)), [yRange]);
+  useEffect(
+    () =>
+      yRange.onChange((v) => {
+        setWidth(v);
+        setIsComplete(v >= 100);
+      }),
+    [yRange]
+  );
 
   return (
     <header className="app-header">
@@ -23,16 +30,14 @@ const Header: React.FC<HeaderProps> = ({ scrollProgress }) => {
         alt="sketchy outline of a hand in red"
       />
       <h1 className="app-title">Hey, You</h1>
-      <svg className="progress-bar" viewBox="0 0 300 1">
-        <motion.path
-          fill="none"
-          strokeWidth="2"
-          stroke={isComplete ? "black" : "white"}
-          strokeDasharray="0 1"
-          d="M 0,0 L 300,0"
-          style={{ pathLength }}
-        />
-      </svg>
+      <motion.div
+        className="progress-bar"
+        style={{
+          height: 4,
+          width: `${width}%`,
+          backgroundColor: isComplete ? "black" : "white",
+        }}
+      />
     </header>
   );
 };
